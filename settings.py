8e14f23f515c867f759c45236951b02c00a62188
Rafael Soutelino <rsoutelino@gmail.com>
2021-10-01 12:58:07 +1300

    abstracting to settings

---
+++ settings.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class RomsNCFiles:
+    grd: str
+    nud: str
+    ini: str
+    bry: str
+    clm: str
+    his: str
+    avg: str
+    qks: str
+    dia: str
+    frc: str
+    rivers: str
+    tides: str
+
+
+firstvar = RomsNCFiles(
+    grd="h",
+    nud="temp_NudgeCoef",
+    ini="temp",
+    bry="temp_west",
+    clm="temp",
+    his="temp",
+    avg="temp",
+    qks="temp",
+    dia="undefined",
+    frc="Pair",
+    rivers="river_salt",
+    tides="tide_Eamp",
+)
